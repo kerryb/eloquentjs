6d819bcc68a2922c8f822c2d9fa80e2b5411338f
Kerry Buckley <kerryjbuckley@gmail.com>
2012-04-23 16:57:46 +0100

    Ex2.5: the incredible 2 + 2 quiz

---
+++ ch2/maths_quiz.js
@@ -0,0 +1,8 @@
+var answer = prompt("What is 2 + 2?");
+if (answer == "4") {
+  alert("Ooh, aren't you clever?");
+} else if (answer == "3" || answer == "5") {
+  alert("Almost!");
+} else {
+  alert("You're an idiot.");
+}
