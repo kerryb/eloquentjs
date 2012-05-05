8f4990291d8d8dd057a8a0714b606709afe6ae27
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-05 09:57:00 +0100

    Ex6.2: processParagraph

---
+++ ch6/processParagraph.js
@@ -0,0 +1,20 @@
+function processParagraph(para) {
+  function findHeadingLevel(para) {
+    if (para[0] === "%") {
+      return 1 + findHeadingLevel(para.slice(1));
+    } else {
+      return 0;
+    }
+  }
+
+  var headingLevel = findHeadingLevel(para);
+  if (headingLevel === 0) {
+    return {"content": para, "type": "p"};
+  } else {
+    return {"content": para.slice(headingLevel + 1), "type": "h" + headingLevel};
+  }
+}
+
+show(processParagraph("Not a heading"));
+show(processParagraph("% Heading one"));
+show(processParagraph("%%%% Heading four"));
