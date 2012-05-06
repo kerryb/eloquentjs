688bee6ad1eab9b76a6d12fd786d8539eb26379f
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-06 20:24:39 +0100

    Add functions from book to helper

--- ch6/spec/javascripts/support/functions.js
+++ ch6/spec/javascripts/support/functions.js
@@ -3,9 +3,26 @@ function forEach(array, action) {
     action(array[i]);
 }
 
+function map(func, array) {
+  var result = [];
+  forEach(array, function (element) {
+    result.push(func(element));
+  });
+  return result;
+}
+
 function reduce(combine, base, array) {
   forEach(array, function (element) {
     base = combine(base, element);
   });
   return base;
+}
+
+function tag(name, content, attributes) {
+  return {name: name, attributes: attributes, content: content};
+}
+
+function footnote(number) {
+  return tag("sup", [link("#footnote" + number,
+                          String(number))]);
 }
