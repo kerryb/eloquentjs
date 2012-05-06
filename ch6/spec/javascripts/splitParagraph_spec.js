9b548693fc5f58175df33845b749315fce5ba496
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-06 09:38:43 +0100

    Ex6.3: splitParagraph

---
+++ ch6/spec/javascripts/splitParagraph_spec.js
@@ -0,0 +1,44 @@
+describe("splitParagraph", function(){
+  it("leaves a simple paragraph unchanged", function(){
+    expect(splitParagraph("a paragraph")).toEqual(
+      [{"content": "a paragraph", "type": "normal"}]);
+  });
+
+  it("extracts an emphasised part at the beginning", function(){
+    expect(splitParagraph("*This* is a paragraph!")).toEqual(
+      [{"content": "This", "type": "emphasis"},
+      {"content": " is a paragraph!", "type": "normal"}]);
+  });
+
+  it("extracts an emphasised part in the middle", function(){
+    expect(splitParagraph("This *is* a paragraph!")).toEqual(
+      [{"content": "This ", "type": "normal"},
+      {"content": "is", "type": "emphasis"},
+      {"content": " a paragraph!", "type": "normal"}]);
+  });
+
+  it("extracts an emphasised part at the end", function(){
+    expect(splitParagraph("This is a *paragraph!*")).toEqual(
+      [{"content": "This is a ", "type": "normal"},
+      {"content": "paragraph!", "type": "emphasis"}]);
+  });
+
+  it("extracts a footnote at the beginning", function(){
+    expect(splitParagraph("{footnote} blah")).toEqual(
+      [{"content": "footnote", "type": "footnote"},
+      {"content": " blah", "type": "normal"}]);
+  });
+
+  it("extracts a footnote in the middle", function(){
+    expect(splitParagraph("blah {footnote} blah")).toEqual(
+      [{"content": "blah ", "type": "normal"},
+      {"content": "footnote", "type": "footnote"},
+      {"content": " blah", "type": "normal"}]);
+  });
+
+  it("extracts a footnote at the end", function(){
+    expect(splitParagraph("blah {footnote}")).toEqual(
+      [{"content": "blah ", "type": "normal"},
+      {"content": "footnote", "type": "footnote"}]);
+  });
+});
---
+++ ch6/splitParagraph.js
@@ -0,0 +1,31 @@
+function splitParagraph(para) {
+  function findNext(str, markers) {
+    for (var i = 0; i < markers.length; i ++) {
+      var next = str.indexOf(markers[i], 1);
+      if (next !== -1) {
+        return next;
+      }
+    }
+    return str.length;
+  }
+
+  var remainder = para;
+  var chunks = [];
+  var end;
+  while (remainder.length !== 0) {
+    if (remainder[0] === "*") {
+      end = findNext(remainder, ["*"]);
+      chunks.push({"content": remainder.slice(1, end), "type": "emphasis"});
+      remainder = remainder.slice(end + 1);
+    } else if (remainder[0] === "{") {
+      end = findNext(remainder, ["}"]);
+      chunks.push({"content": remainder.slice(1, end), "type": "footnote"});
+      remainder = remainder.slice(end + 1);
+    } else {
+      end = findNext(remainder, ["*", "{"]);
+      chunks.push({"content": remainder.slice(0, end), "type": "normal"});
+      remainder = remainder.slice(end);
+    }
+  }
+  return chunks;
+}
