260b34a70693af471b140ef4aebf56e536095e8e
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-21 21:26:40 +0100

    ex8.1: move methods to prototype

--- ch8/Point.js
+++ ch8/Point.js
@@ -1,10 +1,12 @@
 function Point(x, y) {
   this.x = x;
   this.y = y;
-  this.add = function(point) {
-    return new Point(this.x + point.x, this.y + point.y);
-  };
-  this.isEqualTo = function(point) {
-    return this.x === point.x && this.y === point.y;
-  };
 }
+
+Point.prototype.add = function(point) {
+  return new Point(this.x + point.x, this.y + point.y);
+};
+
+Point.prototype.isEqualTo = function(point) {
+  return this.x === point.x && this.y === point.y;
+};
