cd8fff57c8bcd41f05dbbceaea1724e988903dcf
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-27 06:47:12 +0100

    Ex10.2: e-mail regexp

--- ch10/regexps.js
+++ ch10/regexps.js
@@ -1,1 +1,2 @@
 var date_regexp = /\d{2}\/\d{2}\/\d{4}/;
+var email_regexp = /\b[A-Za-z\d.-]+@(?:[A-Za-z\d\-]+\.)+[A-Za-z\d]{2,3}\b/;
--- ch10/spec/javascripts/regexps_spec.js
+++ ch10/spec/javascripts/regexps_spec.js
@@ -3,3 +3,33 @@ describe("date_regexp", function(){
     expect("born 15/11/2003 (mother Spot): White Fang".search(date_regexp)).toEqual(5);
   });
 });
+
+describe("email_regexp", function(){
+  it("matches simplified e-mail addresses", function(){
+    expect("To: kerryjbuckley@gmail.com".search(email_regexp)).toEqual(4);
+  });
+
+  it("allows upper case characters", function(){
+    expect("To: KerryBuckley@Gmail.Com".search(email_regexp)).toEqual(4);
+  });
+
+  it("allows numbers, dots and hyphens", function(){
+    expect("To: kerry.buckley-42@foo-bar123.example.com".search(email_regexp)).toEqual(4);
+  });
+
+  it("does not allow underscores", function(){
+    expect("To: kerry_buckley@example.com".search(email_regexp)).toEqual(-1);
+  });
+
+  it("requires at least two characters in the final domain segment", function(){
+    expect("To: someone@example.c".search(email_regexp)).toEqual(-1);
+  });
+
+  it("requires at most three characters in the final domain segment", function(){
+    expect("To: someone@example.comx".search(email_regexp)).toEqual(-1);
+  });
+
+  it("allows upper case and digits in the final domain segment", function(){
+    expect("To: someone@example.B2B".search(email_regexp)).toEqual(4);
+  });
+});
