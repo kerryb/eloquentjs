18a96cccb92fab6302164b702766135ce71a9983
Kerry Buckley <kerryjbuckley@gmail.com>
2012-05-27 08:11:18 +0100

    Use /i flag instead of having [A-Za-a] everywhere

--- ch10/regexps.js
+++ ch10/regexps.js
@@ -1,2 +1,2 @@
 var date_regexp = /\d{2}\/\d{2}\/\d{4}/;
-var email_regexp = /\b[A-Za-z\d.-]+@(?:[A-Za-z\d\-]+\.)+[A-Za-z\d]{2,3}\b/;
+var email_regexp = /\b[a-z\d.-]+@(?:[a-z\d\-]+\.)+[a-z\d]{2,3}\b/i;
